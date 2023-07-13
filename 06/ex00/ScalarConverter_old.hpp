bcacf8ee4dc23af1372de264a9cc27ecbb7f0f62
lsordo | Luca Sordo <lsordo@student.42heilbronn.de>
2023-07-13 11:00:51 +0200

    Changing 06 	modified:   06/ex00/ScalarConverter.hpp 	new file:   06/ex00/ScalarConverter_old copy.cpp 	renamed:    06/ex00/ScalarConverter.cpp -> 06/ex00/ScalarConverter_old.cpp 	new file:   06/ex00/ScalarConverter_old.hpp

--- 06/ex00/ScalarConverter_old.hpp
+++ 06/ex00/ScalarConverter_old.hpp
@@ -6,7 +6,7 @@
 /*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 10:52:11 by lsordo            #+#    #+#             */
-/*   Updated: 2023/07/13 10:42:36 by lsordo           ###   ########.fr       */
+/*   Updated: 2023/07/11 14:04:48 by lsordo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,11 @@
 # include <cmath>
 # include <math.h>
 # include <iomanip>
-# include <cstdlib>
 # include "Colors.hpp"
 
 # define ERR_CONVERTIBLE	"impossible"
 # define ERR_DISPLAYABLE	"Non displayable"
 # define ERR_ARGNUM			"Wrong number of arguments !"
-# define MSG_CHAR			"char   :"
-# define MSG_INT			"int    :"
-# define MSG_FLOAT			"float  :"
-# define MSG_DOUBLE			"double :"
-
 
 class	ScalarConverter {
 	private:
