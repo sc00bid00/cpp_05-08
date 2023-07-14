5c8fa2518234a4cf73b2da35bb84ed15e42177f5
Luca | lsordo <sordo.luca@gmail.com>
2023-07-14 05:24:39 +0200

    Modify corrected features char and int 	modified:   ScalarConverter.cpp 	modified:   main.cpp

--- 06/ex00/ScalarConverter.cpp
+++ 06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 10:22:06 by lsordo            #+#    #+#             */
-/*   Updated: 2023/07/13 14:27:52 by lsordo           ###   ########.fr       */
+/*   Updated: 2023/07/14 05:23:50 by lsordo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,25 @@ ScalarConverter	&	ScalarConverter::operator=(ScalarConverter const& rhs) {
 }
 
 /* === METHOD IMPLEMENTATION === */
+std::string	checkChar(int n) {
+	std::stringstream	ss;
+	if (n >= 0 && n <= 255 && isprint(n)) {
+		ss << static_cast<char>(n);
+		return ss.str();
+	}
+	if (n >= 0 && n <= 255)
+		return ERR_DISPLAYABLE;
+	return ERR_CONVERTIBLE;
+
+}
+
+void	manageInt(long n) {
+	std::cout <<  MSG_CHAR << checkChar(static_cast<int>(n)) << RESET << std::endl;
+	std::cout <<  MSG_INT << static_cast<int>(n) << RESET << std::endl;
+	std::cout <<  MSG_FLOAT << static_cast<float>(n) << ".0f" << RESET << std::endl;
+	std::cout <<  MSG_DOUBLE << static_cast<double>(n) << ".0" << RESET << std::endl;
+
+}
 
 bool	isNanf(std::string token) {
 	std::string tkn[3] = {"nanf", "+nanf", "-nanf"};
@@ -77,10 +96,10 @@ bool	isInf(std::string token) {
 void	manageSingle(std::string literal) {
 	if (isdigit(literal[0]))
 	{
-		std::cout << BGREEN << MSG_CHAR << literal[0] << RESET << std::endl;
-		std::cout << BGREEN << MSG_INT << static_cast<int>(literal[0]) << RESET << std::endl;
-		std::cout << BGREEN << MSG_FLOAT << static_cast<float>(literal[0]) << ".0f" << RESET << std::endl;
-		std::cout << BGREEN << MSG_DOUBLE << static_cast<double>(literal[0]) << RESET << std::endl;
+		std::cout << MSG_CHAR << literal[0] << RESET << std::endl;
+		std::cout << MSG_INT << static_cast<int>(literal[0]) << RESET << std::endl;
+		std::cout << MSG_FLOAT << static_cast<float>(literal[0]) << ".0f" << RESET << std::endl;
+		std::cout << MSG_DOUBLE << static_cast<double>(literal[0]) << RESET << std::endl;
 		}
 }
 
@@ -90,12 +109,12 @@ void	ScalarConverter::convert(std::string literal) {
 	else {
 		char*	lastChar;
 		long n = strtol(literal.c_str(), &lastChar, 10);
-		if (*lastChar == '\0' && n < INT_MAX && n > INT_MIN)
+		if (*lastChar == '\0' && n < std::numeric_limits<int>::max() && n > std::numeric_limits<int>::min())
 			manageInt(n);
 		else {
 			long double ld = strtold(literal.c_str(), &lastChar);
 			if (*lastChar == 'f')
-				manageFloat(ld);
+				// manageFloat(ld);
 			else if (*lastChar == '\0')
 				manageDouble(ls);
 			else {
@@ -104,4 +123,6 @@ void	ScalarConverter::convert(std::string literal) {
 				std::cerr << BRED << MSG_FLOAT << ERR_CONVERTIBLE << ".0f" << RESET << std::endl;
 				std::cerr << BRED << MSG_DOUBLE << ERR_CONVERTIBLE << RESET << std::endl;
 			}
+		}
+	}
 }
