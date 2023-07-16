c79a430a40f1607b2f524fd34ec0e32291fa76f0
lsordo | Luca Sordo <lsordo@student.42heilbronn.de>
2023-07-16 18:17:13 +0200

    Modify 	modified:   whatever.hpp

--- 07/ex00/whatever.hpp
+++ 07/ex00/whatever.hpp
@@ -6,9 +6,11 @@
 /*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 15:06:32 by lsordo            #+#    #+#             */
-/*   Updated: 2023/07/16 16:14:39 by lsordo           ###   ########.fr       */
+/*   Updated: 2023/07/16 16:19:39 by lsordo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#pragma once
 
 template<typename T>
 void	swap(T& fst, T& snd) {
