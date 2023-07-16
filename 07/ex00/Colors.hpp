8e8dbbf74bfd308f6454233a28bef53d13cc3186
lsordo | Luca Sordo <lsordo@student.42heilbronn.de>
2023-07-16 16:04:16 +0200

    First commit cpp07 - ex00 	new file:   Colors.hpp 	new file:   Makefile 	new file:   main.cpp 	new file:   whatever.hpp

---
+++ 07/ex00/Colors.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Colors.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/24 13:57:29 by lsordo            #+#    #+#             */
+/*   Updated: 2023/07/16 15:31:59 by lsordo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLORS_H
+# define COLORS_H
+
+# define BRED		"\e[1;31m"
+# define BGREEN		"\e[1;32m"
+# define BYELLOW	"\e[1;33m"
+# define BBLUE		"\e[1;34m"
+# define BPURPLE	"\e[1;35m"
+# define BCYAN		"\e[1;36m"
+# define BWHITE		"\e[1;37m"
+# define RESET		"\e[0m"
+# define VERBOSE	false
+
+# include <iostream>
+# include <string>
+
+#endif
---
+++ 07/ex00/whatever.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/16 15:06:32 by lsordo            #+#    #+#             */
+/*   Updated: 2023/07/16 15:59:03 by lsordo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+template<typename T>
+void	swap(T& fst, T& snd) {
+	T	tmp = fst;
+	fst = snd;
+	snd = tmp;
+}
+
+template<typename T>
+T const&	min(T const& x, T const& y) {
+	return (y > x ? x : y);
+}
+
+template<typename T>
+T const&	max(T const& x, T const& y) {
+	return (y < x ? x : y);
+}
